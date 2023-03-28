ce710352b09d110ff7f307601f0a9453e176b3ec
DariaBsk <Dariabsk602@gmail.com>
2023-03-28 07:11:13 +0300

    Добавление домашней работы 6

---
+++ HW6/Program.java
@@ -0,0 +1,19 @@
+package HW6;
+
+import java.util.Scanner;
+import models.zoo.Zoo;
+import presenters.Presenter;
+import views.ConsoleView;
+import views.View;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Zoo javaZoo = new Zoo();
+        View view = new ConsoleView(scan);
+        Presenter presenter = new Presenter(view, javaZoo);
+        presenter.Run();
+        scan.close();
+    }
+}
+
